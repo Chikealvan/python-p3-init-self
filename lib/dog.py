19afb61137d46125bb734053440fd0bb7e7d8bfd
chikealvan <nwadoziechikealvan@gmail.com>
2023-10-05 11:09:47 +0100

    first commit

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
+# Define a Dog class with a constructor that initializes 'name' and 'breed' attributes.
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        self.name = name  # Initialize the 'name' attribute with the provided name.
+        self.breed = breed  # Initialize the 'breed' attribute with the provided breed.
+        
+
+    # Define a method 'bark' to print a message indicating that the dog is barking.
+    def bark(self):
+        print(f"{self.name} is barking.")
+
+    # Define a method 'sit' to print a message indicating that the dog is sitting.
+    def sit(self):
+        print("The dog is sitting.")
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
 
+# Define a Person class with an constructor that initializes name and age attributes.
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+
